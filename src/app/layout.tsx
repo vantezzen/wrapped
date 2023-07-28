62c11342fc2b356d12b5bf3112ddcdc73e158421
Bennett <hello@vantezzen.io>
2023-07-28 17:00:42 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,12 @@ export default function RootLayout({
             referrerPolicy="no-referrer-when-downgrade"
           />
         </noscript>
+
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8508732166185621"
+          crossOrigin="anonymous"
+        ></Script>
       </body>
     </html>
   );
