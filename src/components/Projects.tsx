2d4a06ccfaec68bf98a2487c84172a2bad05f0b7
Bennett <hello@vantezzen.io>
2024-01-04 17:28:56 +0100

    Add more projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,6 +7,12 @@ function Projects() {
       <p className="font-bold text-lg">Check out my other projects:</p>
       <div className="grid grid-cols-2 md:grid-cols-4 mt-6 gap-8">
         <ProjectCard
+          title="Wrapped for Instagram"
+          description="How many stories did you post last year? Find out with Wrapped for Instagram"
+          image="/projects/instagram.png"
+          link="https://ig.vantezzen.io/"
+        />
+        <ProjectCard
           title="Wrapped for Doordash"
           description="How many burgers did you order last year? Find out with Wrapped for Doordash"
           image="/projects/doordash.png"
@@ -29,6 +35,24 @@ function Projects() {
           description="Easily plan your Interrail/Eurail trip across Europe"
           image="/projects/wonderrail.png"
           link="https://wonderrail.com"
+        />
+        <ProjectCard
+          title="Melody Mayhem"
+          description="Let your Spotify playlists battle it out!"
+          image="/projects/melodymayhem.png"
+          link="https://melody-mayhem.vantezzen.io/"
+        />
+        <ProjectCard
+          title="neonFin"
+          description="Discover Your Hidden Spending Habits"
+          image="/projects/neonfin.png"
+          link="https://neonfin.app"
+        />
+        <ProjectCard
+          title="Skip Silence"
+          description="Skip silent parts in video lectures and other videos"
+          image="/projects/skipsilence.png"
+          link="https://vantezzen.io/get/skipsilence"
         />
       </div>
     </div>
